b2a0acc2fbc8edb24295fb64d0c624d2ddf75ad7
Tim Webster <tim.webster@gmail.com>
2018-04-02 19:32:51 +0100

    add files

---
+++ encrypt/encrypt.go
@@ -0,0 +1,59 @@
+package encrypt
+
+import (
+	"errors"
+    "crypto/cipher"
+    "crypto/aes"
+    "crypto/rand"
+    "io"
+)
+
+func encrypt(plaintext string, key string) (string, error) {
+	encrypted, err := encryptBytes([]byte(plaintext), []byte(key))
+	return string(encrypted[:]), err
+}
+
+func decrypt(plaintext string, key string) (string, error) {
+	decryptedBytes, err := decryptBytes([]byte(plaintext), []byte(key))
+	return string(decryptedBytes[:]), err
+}
+
+func encryptBytes(plaintext []byte, key []byte) ([]byte, error) {
+	c, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    gcm, err := cipher.NewGCM(c)
+    if err != nil {
+        return nil, err
+    }
+
+    nonce := make([]byte, gcm.NonceSize())
+    if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+        return nil, err
+    }
+
+    return gcm.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+func decryptBytes(encrypted []byte, key []byte) ([]byte, error) {
+
+    c, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    gcm, err := cipher.NewGCM(c)
+    if err != nil {
+        return nil, err
+    }
+
+    nonceSize := gcm.NonceSize()
+    if len(encrypted) < nonceSize {
+        return nil, errors.New("ciphertext too short")
+    }
+
+    nonce, encrypted := encrypted[:nonceSize], encrypted[nonceSize:]
+    return gcm.Open(nil, nonce, encrypted, nil)
+}
---
+++ encrypt/encrypt_test.go
@@ -0,0 +1,66 @@
+package encrypt
+
+import (
+    "testing"
+    "fmt"
+)
+
+// AES keys
+const aes128Key string = "1234567812345678"
+const aes192Key string = "123456781234567812345678"
+const aes256Key string = "12345678123456781234567812345678"
+
+// The value we're encrypting
+const password string = "mypassword"
+
+func TestEncryptAes128(t *testing.T) {
+    encrypted, _ := encrypt(password, aes128Key)
+    decrypted, _ := decrypt(encrypted, aes128Key)
+
+    if (decrypted != password) {
+        t.Errorf("Fail!  Expected: %s, Actual: %s", password, decrypted)
+    }
+}
+
+func TestEncryptAes128WithWrongKey(t *testing.T) {
+    encrypted, _ := encrypt(password, aes128Key)
+    _, err := decrypt(encrypted, aes192Key)
+
+    if (err == nil) {
+        t.Errorf("Should have failed, wrong key")
+    }
+}
+
+func TestEncryptAes192(t *testing.T) {
+    encrypted, _ := encrypt(password, aes192Key)
+    decrypted, _ := decrypt(encrypted, aes192Key)
+
+    if (decrypted != password) {
+        t.Errorf("Fail!  Expected: %s, Actual: %s", password, decrypted)
+    }
+}
+
+func TestEncryptAes256(t *testing.T) {
+    encrypted, _ := encrypt(password, aes256Key)
+    decrypted, _ := decrypt(encrypted, aes256Key)
+
+    if (decrypted != password) {
+        t.Errorf("Fail!  Expected: %s, Actual: %s", password, decrypted)
+    }
+}
+
+func TestEncryptKeyTooShort(t *testing.T) {
+    _, err := encrypt(password, "12345678")
+    if (err == nil) {
+        t.Error("Test should have errored")
+    }
+    fmt.Printf("PASSED: Caught key size error: %s\n", err)
+}
+
+func TestEncryptKeyTooLong(t *testing.T) {
+    _, err := encrypt(password, "1234567812345678123456781234567812345678")
+    if (err == nil) {
+        t.Error("Test should have errored")
+    }
+    fmt.Printf("PASSED: Caught key size error: %s\n", err)
+}
